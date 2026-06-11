6f55e0906ca5f71350781aae1df246b5344fe834
Karim El-Sharkawy <karimelsharkawy2002@gmail.com>
2026-06-10 21:26:21 -0400

    added some clarifying comments

--- src/forces.py
+++ src/forces.py
@@ -3,11 +3,11 @@ import numpy as np
 def compute_forces(pos, similarity_matrix, alpha, beta):
     F = np.zeros_like(pos)
 
-    for i in range(len(pos)):
-        diff = pos[i] - pos
+    for i in range(len(pos)): # len(pos) == len(words)
+        diff = pos[i] - pos # Shape: (N, 2) - diff to all other words
 
         strength = -alpha * (
-            similarity_matrix[i] - beta
+            similarity_matrix[i] - beta #attract/repel
         )
 
         F[i] = np.sum(
